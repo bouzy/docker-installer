934db2229f217219f180657b14bb0da66e3d28fb
Mo <mo@rsn-ws-01.localdomain>
2023-09-15 00:47:42 -0400

    portainer-agent

--- setup.sh
+++ setup.sh
@@ -18,3 +18,5 @@ sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose
 
 sudo docker volume create portainer_data
 sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ee:latest
+
+#sudo docker run -d -p 9001:9001 --name portainer_agent --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker/volumes:/var/lib/docker/volumes portainer/agent:2.19.0
