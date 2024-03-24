38295642918d5e6ef17a25400d9d0d8c2b7f2966
bouzy <bouzy@users.noreply.github.com>
2024-03-23 22:35:09 -0400

    refactoring

--- setup.sh
+++ setup.sh
@@ -1,35 +1,55 @@
 #!/bin/sh
 
-# Docker installation
+docker_installer(){
+  sudo apt update -y && sudo apt autoremove -y && sudo apt autoclean -y
+  sudo apt install -y ca-certificates curl gnupg lsb-release
 
-sudo apt update -y && sudo apt autoremove -y && sudo apt autoclean -y
-sudo apt install -y ca-certificates curl gnupg lsb-release
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt update
+  sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose
+}
 
-sudo apt update
-sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose
+portainer_installer(){
+  sudo docker volume create portainer_data
 
-# Portainer installation
+  sudo docker run -d \
+    -p 8000:8000 \
+    -p 9443:9443 \
+    --name portainer \
+    --restart=always \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v portainer_data:/data portainer/portainer-ee:latest
+}
 
-sudo docker volume create portainer_data
-sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ee:latest
+portainer_agent_installer(){
+  sudo docker run -d \
+    -p 9001:9001 \
+    --name portainer_agent \
+    --restart=always \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v /var/lib/docker/volumes:/var/lib/docker/volumes \
+    portainer/agent:latest
+}
 
-# sudo docker run -d -p 9001:9001 --name portainer_agent --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker/volumes:/var/lib/docker/volumes portainer/agent:2.19.0
-# sudo docker run -d -p 9001:9001 --name portainer_agent --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker/volumes:/var/lib/docker/volumes portainer/agent:latest
+portainer_agent_update(){
+  sudo docker stop portainer_agent
+  sudo docker rm portainer_agent
 
-# Portainer update
+  sudo docker run -d \
+    -p 9001:9001 \
+    --name portainer_agent \
+    --restart=always \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v /var/lib/docker/volumes:/var/lib/docker/volumes \
+    portainer/agent:latest
+}
 
-# sudo docker stop portainer
-# sudo docker rm portainer
-# sudo docker pull portainer/portainer-ee:latest
-# sudo docker run -d -p 8000:8000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ee:latest
-
-# sudo docker stop portainer_agent
-# sudo docker rm portainer_agent
-# sudo docker pull portainer/agent:latest
-# sudo docker run -d -p 9001:9001 --name portainer_agent --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker/volumes:/var/lib/docker/volumes portainer/agent:latest
+# docker_installer
+# portainer_installer
+# portainer_agent_installer
+# portainer_agent_update
